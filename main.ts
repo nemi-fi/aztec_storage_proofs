c8ba1f39138eb42e3e7fd006665a138e2bc57935
oleh <olehmisar@gmail.com>
2025-05-07 13:29:58 +0100

    works

---
+++ main.ts
@@ -0,0 +1,62 @@
+import "fake-indexeddb/auto";
+// @ts-ignore
+globalThis.self = globalThis;
+process.env.LOG_LEVEL = "warn";
+
+import { getInitialTestAccountsManagers } from "@aztec/accounts/testing";
+import { createAztecNodeClient, Fr, MerkleTreeId, PXE } from "@aztec/aztec.js";
+import {
+  createPXEService,
+  getPXEServiceConfig,
+  type PXEOracleInterface,
+} from "@aztec/pxe/client/lazy";
+import { StorageProofContract } from "./target/StorageProof.js";
+
+async function main() {
+  const node = createAztecNodeClient("http://localhost:8080");
+  const config = getPXEServiceConfig();
+  config.proverEnabled = false;
+  const pxe = await createPXEService(node, config);
+  const accounts = await getInitialTestAccountsManagers(pxe);
+  const alice = await accounts[0].register();
+
+  const contract = await StorageProofContract.deploy(alice).send().deployed();
+  console.log("deployed at", contract.address.toString());
+  const receipt = await contract.methods.set_value(100).send().wait();
+
+  // const contract = await StorageProofContract.at(AztecAddress.fromString(), alice)
+
+  const noteHash = (await contract.methods.get_note2().simulate()) as bigint;
+
+  const membershipWitness = await getMembershipWitness(
+    pxe,
+    receipt.blockNumber!,
+    new Fr(noteHash),
+  );
+  console.log("membershipWitness", membershipWitness);
+
+  // const notes = await pxe.getNotes({
+  //   contractAddress: contract.address,
+  //   txHash: receipt.txHash,
+  // });
+  console.log("note", noteHash);
+  // console.log("notes", notes);
+}
+
+async function getMembershipWitness(
+  pxe: PXE,
+  blockNumber: number,
+  noteHash: Fr,
+) {
+  // TODO: remove type cast
+  const pxe2: PXE & Pick<PXEOracleInterface, "getMembershipWitness"> = (
+    pxe as any
+  ).simulator.executionDataProvider;
+  return await pxe2.getMembershipWitness(
+    blockNumber,
+    MerkleTreeId.NOTE_HASH_TREE,
+    noteHash,
+  );
+}
+
+main();
